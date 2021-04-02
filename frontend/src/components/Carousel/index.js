f9ea696439c833a981325aad40a135ed28727e79
walt <walter.hills5@gmail.com>
2021-04-02 14:33:54 -0500

    programatically rendering carousel

--- frontend/src/components/Carousel/index.js
+++ frontend/src/components/Carousel/index.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Redirect } from "react-router-dom";
 import {
@@ -10,87 +11,47 @@ import {
 import "pure-react-carousel/dist/react-carousel.es.css";
 import "./Carousel.css";
 
-function Carousel() {
+function Carousel({ title, focusId, setFocusId, videos }) {
+  function videoMap(videos) {
+    videos = Object.values(videos);
+    let count = Math.ceil(videos.length / 5);
+    let index = 0;
+    let slideCount = 0;
+    const returnArr = [];
+    while (count) {
+      let slides = [];
+      while (slideCount < videos.length) {
+        slides.push(
+          <img className="slide" src={videos[slideCount]?.imageURL}></img>
+        );
+        slideCount++;
+      }
+      returnArr.push(
+        <Slide className="carouselSlide" index={index}>
+          {slides}
+        </Slide>
+      );
+      index += 1;
+      count--;
+    }
+    return returnArr;
+  }
   return (
     <div className="carouselOuterShell">
       <div className="carouselTitleShell">
-        <div className="carouselTitle">ValHeim</div>
+        <div className="carouselTitle">{title}</div>
       </div>
       <CarouselProvider
         naturalSlideWidth={100}
         naturalSlideHeight={125}
-        totalSlides={4}
+        totalSlides={videos ? Math.ceil(Object.values(videos).length / 5) : 1}
         infinite={true}
         className="carouselInnerShell"
       >
         <ButtonBack className="carouselButton cbLeft">
           <i className="fas fa-angle-left arrowLeft"></i>
         </ButtonBack>
-        <Slider className="carouselSlider">
-          <Slide className="carouselSlide" index={0}>
-            <img
-              className="slide"
-              src="https://netsteambucket.s3.amazonaws.com/header.jpg"
-            ></img>
-            <img
-              className="slide"
-              src="https://netsteambucket.s3.amazonaws.com/header.jpg"
-            ></img>
-            <img
-              className="slide"
-              src="https://netsteambucket.s3.amazonaws.com/header.jpg"
-            ></img>
-            <img
-              className="slide"
-              src="https://netsteambucket.s3.amazonaws.com/header.jpg"
-            ></img>
-            <img
-              className="slide"
-              src="https://netsteambucket.s3.amazonaws.com/header.jpg"
-            ></img>
-          </Slide>
-          <Slide className="carouselSlide" index={1}>
-            <img
-              className="slide"
-              src="https://netsteambucket.s3.amazonaws.com/header.jpg"
-            ></img>
-            <img
-              className="slide"
-              src="https://netsteambucket.s3.amazonaws.com/header.jpg"
-            ></img>
-            <img
-              className="slide"
-              src="https://netsteambucket.s3.amazonaws.com/header.jpg"
-            ></img>
-            <img
-              className="slide"
-              src="https://netsteambucket.s3.amazonaws.com/header.jpg"
-            ></img>
-            <img
-              className="slide"
-              src="https://netsteambucket.s3.amazonaws.com/header.jpg"
-            ></img>
-          </Slide>
-          <Slide className="carouselSlide" index={2}>
-            <img
-              className="slide"
-              src="https://netsteambucket.s3.amazonaws.com/header.jpg"
-            ></img>
-            <img
-              className="slide"
-              src="https://netsteambucket.s3.amazonaws.com/header.jpg"
-            ></img>
-            <img
-              className="slide"
-              src="https://netsteambucket.s3.amazonaws.com/header.jpg"
-            ></img>
-            <img
-              className="slide"
-              src="https://netsteambucket.s3.amazonaws.com/header.jpg"
-            ></img>
-          </Slide>
-          <Slide className="carouselSlide" index={3}></Slide>
-        </Slider>
+        <Slider className="carouselSlider">{videos && videoMap(videos)}</Slider>
         <ButtonNext className="carouselButton cbRight">
           <i className="fas fa-angle-right arrowRight"></i>
         </ButtonNext>
--- frontend/src/components/HomeFocus/index.js
+++ frontend/src/components/HomeFocus/index.js
@@ -1,56 +1,70 @@
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Redirect } from "react-router-dom";
+import Carousel from "../Carousel";
 import "./HomeFocus.css";
 
 function HomeFocus() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
-  let videoOne = useSelector((state) => state.videos[1]);
+  let videos = useSelector((state) => state.videos);
+  const [focusId, setFocusId] = useState("1");
+  let videoOne = useSelector((state) => state.videos[focusId]);
   if (!sessionUser) {
     return <Redirect to="/" />;
   }
   return (
-    <div className="homeFocusShell">
-      <div className="focusTitleBox">
-        <div className="focusTitle">{videoOne?.title}</div>
-      </div>
-      <div className="focusInnerShell">
-        <div className="videoOuterShell">
-          <iframe
-            className="focusVideo"
-            width="100%"
-            height="100%"
-            src={videoOne?.embedURL}
-            frameborder="0"
-            allow="accelerometer; encrypted-media; gyroscope;"
-            allowFullScreen="allowfullscreen"
-          ></iframe>
+    <>
+      <div className="homeFocusShell">
+        <div className="focusTitleBox">
+          <div className="focusTitle">{videoOne?.title}</div>
         </div>
-        <div className="focusAboutShell">
-          <img className="gameImg" src={videoOne?.imageURL}></img>
-          <div className="aboutVideoText">{videoOne?.about}</div>
-          <div className="reviewOuterShell">
-            <div className="reviewTitleBox">
-              <div className="reviewTitles">All Reviews:</div>
-              <div className="reviewTitles">Release Date:</div>
-              <div className="reviewTitles">Developer:</div>
-              <div className="reviewTitles">Publisher:</div>
-              <div className="reviewTitles reviewGenre">Genre:</div>
-            </div>
-            <div className="reviewDataBox">
-              <div className="reviewData">Overwhelmingly Positive (70,448)</div>
-              <div className="reviewData">Feb 2, 2021</div>
-              <div className="reviewData">{videoOne?.developer}</div>
-              <div className="reviewData">{videoOne?.publisher}</div>
-              <button className="genreButton">Survival</button>
-              <div className="focusButtonShell">
-                <div className="modalFooterText">Show more</div>
+        <div className="focusInnerShell">
+          <div className="videoOuterShell">
+            <iframe
+              className="focusVideo"
+              width="100%"
+              height="100%"
+              src={videoOne?.embedURL}
+              frameborder="0"
+              allow="accelerometer; encrypted-media; gyroscope;"
+              allowFullScreen="allowfullscreen"
+            ></iframe>
+          </div>
+          <div className="focusAboutShell">
+            <img className="gameImg" src={videoOne?.imageURL}></img>
+            <div className="aboutVideoText">{videoOne?.about}</div>
+            <div className="reviewOuterShell">
+              <div className="reviewTitleBox">
+                <div className="reviewTitles">All Reviews:</div>
+                <div className="reviewTitles">Release Date:</div>
+                <div className="reviewTitles">Developer:</div>
+                <div className="reviewTitles">Publisher:</div>
+                <div className="reviewTitles reviewGenre">Genre:</div>
+              </div>
+              <div className="reviewDataBox">
+                <div className="reviewData">
+                  Overwhelmingly Positive (70,448)
+                </div>
+                <div className="reviewData">Feb 2, 2021</div>
+                <div className="reviewData">{videoOne?.developer}</div>
+                <div className="reviewData">{videoOne?.publisher}</div>
+                <button className="genreButton">Survival</button>
+                <div className="focusButtonShell">
+                  <div className="modalFooterText">Show more</div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <Carousel
+        title={"Most Recent"}
+        focusId={focusId}
+        setFocusId={setFocusId}
+        videos={videos}
+      />
+    </>
   );
 }
 export default HomeFocus;
